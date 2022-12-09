704d8ae22d2f9ab6bd64c9447fbf225b9636eb97
ekatsnelson <elisek222@gmail.com>
2022-12-09 20:01:00 +0000

    Added associations and validations for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,14 @@
 #  story_id     :integer
 #
 class Comment < ApplicationRecord
+
+  # Validations
+  validates(:story_id, { :presence => true })
+  validates(:commenter_id, { :presence => true })
+  validates(:body, { :presence => true })
+
+  # Direct Associations
+  belongs_to(:commenter, { :required => true, :class_name => "User", :foreign_key => "commenter_id" })
+
+  belongs_to(:photo, { :required => true, :class_name => "Story", :foreign_key => "story_id", :counter_cache => true })
 end
