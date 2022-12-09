2e1545681910cff224b0a1fdb53e7fc226527bf0
ekatsnelson <elisek222@gmail.com>
2022-12-09 19:53:46 +0000

    Added associations and validations for Stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -14,4 +14,22 @@
 #  owner_id       :integer
 #
 class Story < ApplicationRecord
+
+  # Validations
+  validates(:story, { :presence => true })
+  validates(:owner_id, { :presence => true })
+
+  # Direct Associations
+  belongs_to(:owner, { :required => true, :class_name => "User", :foreign_key => "owner_id", :counter_cache => :own_photos_count })
+
+  has_many(:likes, { :class_name => "Like", :foreign_key => "story_id", :dependent => :destroy })
+
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "story_id", :dependent => :destroy })
+
+  # Indirect Associations
+  has_many(:fans, { :through => :likes, :source => :user })
+
+  has_many(:followers, { :through => :owner, :source => :following })
+
+  has_many(:fan_followers, { :through => :fans, :source => :following })
 end
