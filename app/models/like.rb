9fe7a1456c73ec3e54dd70ec8a557b1b570066c7
ekatsnelson <elisek222@gmail.com>
2022-12-09 19:55:45 +0000

    added associations and validations for Likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,15 @@
 #  user_id    :integer
 #
 class Like < ApplicationRecord
+
+  # Validations
+  validates(:user_id, { :presence => true })
+  validates(:story_id, { :presence => true })
+
+  validates(:story_id, { :uniqueness => { :scope => ["user_id"], :message => "already liked" } })
+
+  # Direct Associations
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
+
+  belongs_to(:photo, { :required => true, :class_name => "Story", :foreign_key => "story_id", :counter_cache => true })
 end
