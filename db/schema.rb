57aa684080a8c8142adae13c7b3482a13437eb47
ekatsnelson <elisek222@gmail.com>
2022-12-09 20:00:01 +0000

    Generated resources for Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_09_195753) do
+ActiveRecord::Schema.define(version: 2022_12_09_195941) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "commenter_id"
+    t.integer "story_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "follow_requests", force: :cascade do |t|
     t.integer "sender_id"
